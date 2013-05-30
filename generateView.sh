0dc8d187272c4cf1d0500958acbcb3788063837b
SquarY <yycosammx@gmail.com>
2013-05-30 11:56:11 +0800

    MOD:Add comment for script

--- generateView.sh
+++ generateView.sh
@@ -1,1 +1,2 @@
+#$1 trace log $2 template file $3 html view file
 java -cp target/*dependencies.jar com.renren.traceview.ViewGenerator $1 $2 $3
--- startTrace.sh
+++ startTrace.sh
@@ -1,1 +1,2 @@
+#1 pid for jvm you want to monitor.#2 claspath for the process your specified in $1
 btrace -cp lib:$2 $1 CallInfoOutput.java
